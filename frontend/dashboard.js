cd175a6cdd3eb9ab8e422148437fd907635e3202
Alex Arts <alexsimons.lc@gmail.com>
2026-05-01 17:41:16 +0530

    Gemini changes to the Dashboard

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -152,7 +152,7 @@ function renderSidebar(activePage) {
           <path d="M13 2L3 14h9l-1 8 10-12h-9l1-8z"/>
         </svg>
       </div>
-      <span class="logo-name">Honou<span>rix</span></span>
+      <span class="logo-name">Gal<span>Sol</span></span>
     </div>
     <nav class="sidebar-nav">
 
